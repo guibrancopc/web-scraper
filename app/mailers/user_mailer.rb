950945b873749c0d9bd3a392c157326e831a0cf4
Guilherme Carvalho <guibrancopc@gmail.com>
2024-09-26 21:05:40 -0300

    feat: setup a rake test to run check and email notification

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.success.subject
+  #
+  def success
+    # @greeting = "Hi"
+    @page = params[:result].page
+
+    mail to: "guibrancopc@gmail.com"
+  end
+end
