07af66a058ca651887f8635e3b9a1f25ffd157ee
Guilherme Carvalho <guibrancopc@gmail.com>
2024-09-26 21:41:43 -0300

    draft: attempt to send email from gmail smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,8 +74,24 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener_web
+
+  # config.action_mailer.delivery_method = :smtp
+  # ActionMailer::Base.raise_delivery_errors = true
+
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'localhost',
+  #   user_name:            'something@gmail.com',
+  #   password:             'pass',
+  #   authentication:       :plain,
+  #   enable_starttls_auto: true,
+  #   open_timeout:         5,
+  #   read_timeout:         5
+  # }
 end
