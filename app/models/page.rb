b2c3211cb43fe51d89c39071a36231af33c61488
Guilherme Carvalho <guibrancopc@gmail.com>
2024-11-08 12:15:17 -0300

    refactor: create a service to abstract the email sending logic

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,4 @@
 class Page < ApplicationRecord
-    include EmailHelper
-
     belongs_to :last_result, class_name: "Result", optional: true
 
     has_many :results
@@ -14,22 +12,7 @@ class Page < ApplicationRecord
         subject = "Web Crawler: Last update of your page named #{result.page.name}"
         content = "The last result for your page #{result.page.name} was #{result.success ? 'SUCCESSFULL' : 'FAILURE'}!"
 
-        from = SendGrid::Email.new(email: 'web-scrapper@guicarvalho.com.br')
-        to = SendGrid::Email.new(email: 'guibrancopc@gmail.com')
-        send_grid_content = SendGrid::Content.new(type: 'text/plain', value: content)
-
-        mail = SendGrid::Mail.new(from, subject, to, send_grid_content)
-
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-
-        puts '&%&%&&%&%&%&%&%&&%&%&%&%&%&%&%&%&%&%&%&%&&%&%'
-        puts response.status_code
-        puts response.body
-        # puts response.parsed_body
-        puts response.headers
-        puts '&%&%&&%&%&%&%&%&&%&%&%&%&%&%&%&%&%&%&%&%&&%&%'
+        EmailService.new.dispatch_email_helper(subject, content)
     end
 
     def check_and_notify
---
+++ app/services/email_service.rb
@@ -0,0 +1,18 @@
+class EmailService
+    def initialize
+        @from = 'web-scrapper@guicarvalho.com.br'
+        @to = 'guibrancopc@gmail.com'
+    end
+
+    def dispatch_email_helper(subject, content)
+        send_grid_from = SendGrid::Email.new(email: @from)
+        send_grid_to = SendGrid::Email.new(email: @to)
+        send_grid_content = SendGrid::Content.new(type: 'text/plain', value: content)
+
+        mail = SendGrid::Mail.new(send_grid_from, subject, send_grid_to, send_grid_content)
+
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+    end
+end
